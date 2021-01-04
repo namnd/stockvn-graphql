a50673c5ead389223bee3136bc80310bf0ac24c1
Nam Nguyen <namnd86@gmail.com>
2021-01-04 22:28:53 +1100

    clean up old http handler

--- server.go
+++ server.go
@@ -30,9 +30,6 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	// http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	// http.Handle("/query", srv)
-
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
 
